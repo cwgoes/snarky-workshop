a4aa2e348d15461bc92a53e445e2464a2bcd36ec
Christopher Goes <cwgoes@pluranimity.org>
2019-06-19 14:49:51 +0200

    more merklizing

--- gpv.sh
+++ gpv.sh
@@ -6,6 +6,6 @@ PROOF=proof
 echo "Running with file: $1, arguments: $ARGS"
 
 rm -f *.pv *.sk *.zkp
-snarky_cli generate-keys $FILE --curve Bn128
-snarky_cli prove $FILE $ARGS
-snarky_cli verify $FILE $ARGS --proof $(ls *.zkp)
+time snarky_cli generate-keys $FILE --curve Bn128
+time snarky_cli prove $FILE $ARGS
+time snarky_cli verify $FILE $ARGS --proof $(ls *.zkp)
