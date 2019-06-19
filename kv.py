1092264a5ffdc419262cec0205ff554bddce05c1
Christopher Goes <cwgoes@pluranimity.org>
2019-06-19 16:30:48 +0200

    key_range

--- kv.py
+++ kv.py
@@ -11,5 +11,35 @@ def pederson_hash(val):
     output = proc.stdout.read()[:-1]
     return output
 
+def flatten(l):
+    return [x for y in l for x in y]
+
+def key_range(depth):
+    if depth == 1:
+        return [[0], [1]]
+    recur = key_range(depth - 1)
+    ret = []
+    for k in recur:
+        ret += [[0] + k]
+        ret += [[1] + k]
+    return ret
+
+class SparseMerkleTree():
+
+    def __init__(self, depth = 3):
+        self.depth = depth
+        self.dict = {}
+
+    def root_hash(self):
+        ## todo cache me
+        return 0
+   
+    def get(key):
+        return ''
+
+    def set(key, value):
+        return
+
 if __name__ == '__main__':
+    print(key_range(3))
     print(pederson_hash('hello world'))
