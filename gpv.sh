befbf43a9e7b54dff7962609e66c91c2dd3c237d
Christopher Goes <cwgoes@pluranimity.org>
2019-06-19 15:25:08 +0200

    build first

--- gpv.sh
+++ gpv.sh
@@ -2,10 +2,12 @@ set -e
 
 FILE=$1
 ARGS=$2
+CLI=/home/cwgoes/temporary/snarky/_build/install/default/bin/snarky_cli
 PROOF=proof
 echo "Running with file: $1, arguments: $ARGS"
 
 rm -f *.pv *.sk *.zkp
-time snarky_cli generate-keys $FILE --curve Bn128
-time snarky_cli prove $FILE $ARGS
-time snarky_cli verify $FILE $ARGS --proof $(ls *.zkp)
+time $CLI build $FILE --curve Bn128
+time $CLI generate-keys $FILE --curve Bn128
+time $CLI prove $FILE $ARGS
+time $CLI verify $FILE $ARGS --proof $(ls *.zkp)
