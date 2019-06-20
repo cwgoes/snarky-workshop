059b9e4391baf9a84516cb27f74ffabfa77a53bd
Christopher Goes <cwgoes@pluranimity.org>
2019-06-20 19:10:46 +0200

    update kv.py

--- kv.py
+++ kv.py
@@ -57,14 +57,28 @@ class SparseMerkleTree():
     def key_range(self):
         return key_range(self.depth)
 
-    def root_hash(self):
+    def calc_all(self):
         hashes = {}
         for k in self.key_range():
             hashes[k] = pedersen_hash(self.get(k))
         for depth in range(1, self.depth)[::-1]:
             for k in key_range(depth):
                 hashes[k] = pedersen_hash(hashes[k + '0'][:TRUNCATE] + hashes[k + '1'][:TRUNCATE])
-        return pedersen_hash(hashes['0'][:TRUNCATE] + hashes['1'][:TRUNCATE])
+        hashes['root'] = pedersen_hash(hashes['0'][:TRUNCATE] + hashes['1'][:TRUNCATE])
+        return hashes
+
+    def root_hash(self):
+        return self.calc_all()['root']
+
+    def merkle_path(self, key):
+        hashes = self.calc_all()
+        path = []
+        for depth in range(0, self.depth)[::-1]:
+            mod = [x for x in key][:depth + 1]
+            mod[depth] = '0' if key[depth] == '1' else '1'
+            mod = ''.join(mod)
+            path.append(hashes[mod])
+        return (path, hashes['root'])
    
     def get(self, key):
         return self.dict[key]
@@ -75,5 +89,8 @@ class SparseMerkleTree():
 if __name__ == '__main__':
     t = SparseMerkleTree()
     print('Root hash: {}'.format(t.root_hash()))
-    t.set('001', '123')
-    print('Root hash after update: {}'.format(t.root_hash()))
+    key = '000'
+    value = '123'
+    print('Setting {} to {}'.format(key, value))
+    t.set(key, value)
+    print('Merkle path and root: {}'.format(t.merkle_path(key)))
